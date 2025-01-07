6237c0bbab9cf0b58a06c218a24ea03fc1df9040
atiamars <atiamars@163.com>
2025-01-07 17:09:16 +0800

    004

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="app">
+    <h1>hello world</h1>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+}
+</script>
 
 <style scoped>
 header {
