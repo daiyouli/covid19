41be99ff86f2e13b2875deecd5e7f5c3d1320281
atiamars <atiamars@163.com>
2025-01-07 21:15:24 +0800

    setupgeneral

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="app">
-    <h1>hello world</h1>
+    <Person />
   </div>
 </template>
 
 <script lang="ts">
+import Person from './components/Person.vue'
 export default {
   name: 'app',
+  components: { Person }, //register components
 }
 </script>
 
---
+++ src/components/Person.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名称</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  setup() {
+    let name = '张三'
+    let age = 18
+    let tel = '1233546678'
+    function changeName() {
+      name = 'daiyouli'
+    }
+    function changeAge() {
+      age += 1
+    }
+    function showTel() {
+      alert(tel)
+    }
+    return { name, age, changeName, changeAge, showTel }
+  },
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: cadetblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
